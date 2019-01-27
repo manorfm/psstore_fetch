dc280c111dcf419e489184c89e29259bab82ee2c
Manoel Rodrigo Farinha de Medeiros <manorfm@hotmail.com>
2019-01-27 00:54:05 -0300

    changing the way of do loggin

--- main.go
+++ main.go
@@ -2,32 +2,43 @@ package main
 
 import (
     "fmt"
+    "log"
     "os"
     "psstore/convert"
     "psstore/file"
     "psstore/search"
     "strconv"
-    "github.com/goinggo/tracelog"
 )
 
-func main() {
-    fmt.Println("Starting the application...")
-    tracelog.StartFile(tracelog.LevelTrace, "./log", 1)
+var (
+    logFile  *os.File
+)
 
-    tracelog.Start(tracelog.LevelTrace)
-
-    argsWithProg := os.Args
-    if len(argsWithProg) != 3 {
-        exiting("Error: Needed the path and the amount by pagination args.", 1)
+func init() {
+    logFile, err := os.OpenFile("./psstore.log", os.O_RDWR | os.O_CREATE | os.O_APPEND, 0666)
+    if err != nil {
+        log.Fatalf("error opening file: %v", err)
     }
 
-    path := argsWithProg[1]
-    itemsPerPageStr := argsWithProg[2]
+    log.SetOutput(logFile)
+}
+
+func main() {
+    defer logFile.Close()
+
+    args := os.Args
+
+    if len(args) != 3 {
+        exiting("Error: Empty args, I need PS api path and pagination amount by args.", 1)
+    }
+
+    path := args[1]
+    itemsPerPageStr := args[2]
     
     itemsPerPage, err := strconv.Atoi(itemsPerPageStr)
     
     if err != nil {
-        exiting("Error: Inform a number to amount for pagination as parameter", 2)
+        exiting("Error: Pagination amount has to be numerical.", 2)
     }
     
     games, err := search.Execute(path, itemsPerPage)
@@ -36,14 +47,13 @@ func main() {
         exiting(fmt.Sprintf("Error while execute search to path %s, %v", path, err), 3)
     }
 
-    file.Write(convert.ToFileStructureGames(games))
+    log.Printf("Fetched a total of %d games", len(games), )
 
-    fmt.Println("End the application...")
+    file.Write(convert.ToFileStructureGames(games))
 }
 
 func exiting(message string, code int) {
-    tracelog.Errorf(fmt.Errorf(message), "main", "main", "Hello Error")
-    tracelog.Stop()
-    os.Exit(1)
+    log.Println(message)
+    os.Exit(code)
 }
 
--- search/search.go
+++ search/search.go
@@ -2,8 +2,8 @@ package search
 
 import (
     "encoding/json"
-    "github.com/goinggo/tracelog"
     "io/ioutil"
+    "log"
     "net/http"
     "psstore/util"
     "time"
@@ -42,8 +42,7 @@ func Execute(path string, itemsPerPage int) ([]Game, error) {
 
     API := API{Client: client, URL: path}
 
-    tracelog.Info("PsStore", "Execute",
-        "executing search starting at %d with %d games at path %s", start, itemsPerPage, path)
+    log.Printf("executing search starting at %d with %d games at path %s", start, itemsPerPage, path)
 
     return execute(&API)
 }
@@ -55,26 +54,20 @@ func execute(api *API) ([]Game, error) {
         return nil, err
     }
 
-    tracelog.Info("PsStore", "execute","found %d games", result.Total)
-
     var games = result.Games
     if hasNext(result.Start, result.Size, result.Total) {
         var start, size int = next(result.Start, result.Size, result.Total);
         time.Sleep(6 * time.Second)
 
         path := api.URL
-        util.UpdatePathPagination(&path, start, size)
+        util.UpdatePathPagination(&path, start, size + 1)
 
-        tracelog.Info("PsStore", "execute",
-            "executing search starting at %d with %d games at path %s", start, size, path)
+        log.Printf("Executing search starting at %d and fetching more %d games of %d at path %s", start, size, result.Total, path)
 
-        nextGames, err := execute(&API{Client: api.Client, URL: path})
+        var nextGames, err = execute(&API{Client: api.Client, URL: path})
         if err != nil {
             return nil, err
         }
-
-        tracelog.Info("PsStore", "execute",
-            "append more %d games found", len(nextGames))
 
         return append(games, nextGames...), nil
     }
